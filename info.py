a5a873c8e75fded8cc6a6754dc848442b3aa564f
Tv show <businesskaro0565@gmail.com>
2026-07-16 18:48:02 +0530

    Set default values for VERIFY_IMG and SHORTENER variables

--- info.py
+++ info.py
@@ -45,9 +45,9 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/TvshowChat') #Support g
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "")
-VERIFY_IMG = environ.get("VERIFY_IMG", "")
-SHORTENER_API = environ.get("SHORTENER_API", "")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", '')
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://i.postimg.cc/QjDYhVZq/6223ff48-88ab-43f0-82d2-67c50b24f3e2.jpg?dl=1")
+SHORTENER_API = environ.get("SHORTENER_API", "f46f4187dd7e10bee91f805cf6214b4381ebdf45")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'gplinks.com')
 SHORTENER_API2 = environ.get("SHORTENER_API2", "")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", '')
 SHORTENER_API3 = environ.get("SHORTENER_API3", "")
